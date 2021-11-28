ac1fc34c46495fc50f07a6591f6fa4b587729fb3
Jakub <jakub.sosna@icloud.com>
2021-11-29 00:22:42 +0100

    Create onboarding page base with vertical indicators system

--- lib/presentation/onboarding/onboarding_page.dart
+++ lib/presentation/onboarding/onboarding_page.dart
@@ -1,15 +1,62 @@
+import 'package:checklist/widgets/vertical_page_indicators.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingPage extends StatefulWidget {
-  const OnboardingPage({ Key? key }) : super(key: key);
+  const OnboardingPage({Key? key}) : super(key: key);
 
   @override
   _OnboardingPageState createState() => _OnboardingPageState();
 }
 
 class _OnboardingPageState extends State<OnboardingPage> {
+  final _pageController = PageController();
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("Onboarding page")));
+    return Scaffold(
+        body: SafeArea(
+      child: Stack(
+        children: [
+          _buildPageView(),
+          VerticalPageIndicators(
+              controller: _pageController, indicatorsCount: 3)
+          // _buildControls(),
+        ],
+      ),
+    ));
+  }
+
+  Widget _buildPageView() {
+    return PageView(
+      scrollDirection: Axis.vertical,
+      physics: const BouncingScrollPhysics(),
+      controller: _pageController,
+      onPageChanged: (page) {
+        setState(() {});
+      },
+      children: [
+        _buildFirstPage(),
+        _buildSecondPage(),
+        _buildThirdPage(),
+      ],
+    );
+  }
+
+  Widget _buildFirstPage() {
+    return Center(child: Text("1"));
+  }
+
+  Widget _buildSecondPage() {
+    return Center(child: Text("2"));
+  }
+
+  Widget _buildThirdPage() {
+    return Center(child: Text("3"));
   }
 }
---
+++ lib/widgets/vertical_page_indicators.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class VerticalPageIndicators extends StatefulWidget {
+  final PageController controller;
+  final int indicatorsCount;
+
+  const VerticalPageIndicators({
+    Key? key,
+    required this.controller,
+    required this.indicatorsCount,
+  }) : super(key: key);
+
+  @override
+  _VerticalPageIndicatorsState createState() => _VerticalPageIndicatorsState();
+}
+
+class _VerticalPageIndicatorsState extends State<VerticalPageIndicators> {
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(
+              widget.indicatorsCount,
+              (index) =>
+                  _buildIndicator(index == widget.controller.page?.round(), () {
+                    setState(() {
+                      widget.controller.animateToPage(index,
+                          duration: const Duration(milliseconds: 400),
+                          curve: Curves.easeInOut);
+                    });
+                  }))),
+    );
+  }
+
+  Widget _buildIndicator(bool isActive, Function() onPressed) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 180),
+          width: 11.0,
+          height: isActive ? 42.0 : 11.0,
+          decoration: BoxDecoration(
+              color: isActive ? Colors.blue.shade400 : Colors.blue[50],
+              borderRadius: BorderRadius.circular(6),
+              border: Border.all(color: Colors.blue.shade400, width: 2)),
+        ),
+      ),
+    );
+  }
+}
