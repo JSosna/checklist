ad70fb44a17e1b60e41a7c05924531bcc24827cd
Jakub <jakub.sosna@icloud.com>
2021-11-29 22:19:30 +0100

    Create onboarding control buttons

--- lib/extension/context_extension.dart
+++ lib/extension/context_extension.dart
@@ -1,6 +1,9 @@
+import 'package:checklist/style/typography.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 extension ContextExtensions on BuildContext {
+  AppTypography get typo => AppTypography();
+  
   bool get isDarkTheme => Theme.of(this).brightness == Brightness.dark;
 }
