28a9f966f18053632c4acb5ddc3de2e43eeb8e52
Jakub <jakub.sosna@icloud.com>
2021-12-19 17:03:23 +0100

    Improve checklist item design

--- lib/widgets/checklist_list_item.dart
+++ lib/widgets/checklist_list_item.dart
@@ -1,3 +1,4 @@
+import 'package:checklist/extension/context_extensions.dart';
 import 'package:checklist/style/dimens.dart';
 import 'package:flutter/material.dart';
 
@@ -17,13 +18,17 @@ class ChecklistListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       borderRadius: BorderRadius.circular(8),
-      color: Colors.grey.withOpacity(0.5),
+      color: context.isDarkTheme
+          ? Colors.grey[850]?.withOpacity(0.5)
+          : Colors.blueGrey[200]?.withOpacity(0.5),
       child: InkWell(
+        borderRadius: BorderRadius.circular(8),
         onTap: onPressed,
         child: SizedBox(
           height: height,
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: Dimens.marginLarge, vertical: Dimens.marginSmall),
+            padding: const EdgeInsets.symmetric(
+                horizontal: Dimens.marginLarge, vertical: Dimens.marginSmall),
             child: child,
           ),
         ),
